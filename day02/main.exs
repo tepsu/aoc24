0745ad01e4a1c7ffdbd104358bb2af3e1681fa5c
Roberto Furduc <t3psukka@gmail.com>
2024-12-04 01:32:57 +0200

    I still don't get it

--- day02/main.exs
+++ day02/main.exs
@@ -12,12 +12,9 @@ defmodule Day02 do
   end
 
   def is_safe(report) do
-    # IO.inspect(report, label: "Report")
-
     {all_asc, all_desc, all_within_margin} =
       report
       |> Enum.chunk_every(2, 1, :discard)
-      # |> IO.inspect(label: "Chunked")
       |> Enum.reduce({true, true, true}, fn
         # NOTE: [a,b] here is coming from chunk_every, as the first argument for .reduce
         # {asc, desc, within} is the accumulator for .reduce
@@ -26,7 +23,6 @@ defmodule Day02 do
       end)
 
     is_safe = (all_asc || all_desc) && all_within_margin
-    # IO.inspect(is_safe, label: "Is safe")
     is_safe
   end
 
@@ -39,11 +35,8 @@ defmodule Day02 do
       :timer.tc(fn ->
         File.stream!("day02.input")
         |> Stream.map(&Day02.parse_line/1)
-        # |> IO.inspect(label: "Parsed line")
         |> Stream.filter(&Day02.is_safe/1)
-        # |> IO.inspect(label: "Filtered")
         |> Enum.to_list()
-        # |> IO.inspect(label: "As list")
         |> Enum.count()
       end)
 
@@ -52,17 +45,30 @@ defmodule Day02 do
   end
 
   def run_part2 do
-    # TODO
-    # {time, result} =
-    #   :timer.tc(fn ->
-    #     File.stream!("day02.sample")
-    #     |> Stream.map(&Day02.parse_line/1)
-    #   end)
+    {time, result} =
+      :timer.tc(fn ->
+        File.stream!("day02.input")
+        |> Stream.map(fn report_line ->
+          report_line
+          |> parse_line()
+          |> then(fn report ->
+            report
+            |> Stream.with_index()
+            |> Stream.map(fn {_num, index} ->
+              report
+              |> List.delete_at(index)
+              |> is_safe()
+            end)
+            |> Enum.any?()
+          end)
+        end)
+        |> Enum.frequencies()
+      end)
 
-    # IO.puts("PART TWO: Execution time: #{time / 1000} ms")
-    # IO.puts("PART TWO: Result: #{result}")
+    IO.puts("PART TWO: Execution time: #{time / 1000} ms")
+    IO.puts("PART TWO: Result: #{result[true]}")
   end
 end
 
-Day02.run_part1()
-# Day02.run_part2()
+# Day02.run_part1()
+Day02.run_part2()
